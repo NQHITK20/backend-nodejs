b653c6738c1728efc7d18fc76566eeb05d48aa84
NQH <normanlovehislife@gmail.com>
2023-04-29 01:35:00 +0700

    ss

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,20 @@
 let handleLogin = (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
+    if (!email || !password) {
+        return res.status(500).json({
+            errCode: 1,
+            message: 'Missing input parameter!'
+        })
+    }
+    //check email
+    //compare password
+    //return userinfo
+    //acess token:JWT
     return res.status(200).json({
-        message: 'vú vú'
+        errCode: 0,
+        message: 'vú vú',
+        yourEmail: email
     })
 }
 
